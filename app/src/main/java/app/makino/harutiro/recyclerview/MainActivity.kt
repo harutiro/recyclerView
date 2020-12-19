ae71f229ca0925ec6466d384b7422c466ae38fb2
harutiro <74090756+harutiro@users.noreply.github.com>
2020-12-19 18:00:22 +0900

    Update MainActivity.kt

--- app/src/main/java/app/makino/harutiro/recyclerview/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/recyclerview/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val adapter = RecyclerViewAdapter(this)
+//      recyclerViewのIDを呼んであげないといけない  
         recycleView.layoutManager = LinearLayoutManager(this)
         recycleView.adapter = adapter
 
